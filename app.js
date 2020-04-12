94ff29df94ee15fc6ce81b515a23198561329d8f
travis.vela <tvela@appddictionstudio.com>
2020-04-12 17:03:28 -0500

    add db connection mongo db and post, get, delete methods

--- app.js
+++ app.js
@@ -1,44 +1,72 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Post = require('./models/post');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+var uri = process.env.MONGO_URI;
+mongoose.connect(encodeURI(uri))
+    .then(res => {
+        console.log('Connected to the database');
+    })
+    .catch(err => {
+        console.log('Connection failed');
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Cotrol-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Cotrol-Allow-Methods', '*')
+//     next();
+// })
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-})
+}
+app.use(allowCrossDomain);
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
-    console.log('🤖 post', post);
-    res.status(201).json({
-        message: 'post added successfully'
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
     });
+    console.log('🤖 post', post);
+    post.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'post added successfully',
+            postId: result._id
+        });
+    });
+
 })
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        {
-            id: 'sdifh73',
-            title: 'First server side post',
-            content: 'this is coming from the server'
-        },
-        {
-            id: 'lkajsdfjelfe',
-            title: 'Second server side post',
-            content: 'This too is coming from the server!'
-        }
-    ]
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Posts fetched successfully',
+                posts: documents
+            });
+        });
+});
 
-    res.status(200).json({
-        message: 'Posts fetched successfully',
-        posts: posts
+app.delete('/api/posts/:id', (req, res, next) => {
+
+    Post.deleteOne({ _id: req.params.id }).then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: 'Post deleted'
+        });
     });
-})
+});
 
 module.exports = app;
