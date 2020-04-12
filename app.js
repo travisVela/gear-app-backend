4f50f99d52be3f75dd377c25deaf672060b9a79b
travis.vela <tvela@appddictionstudio.com>
2020-04-12 18:37:02 -0500

    add update post method

--- app.js
+++ app.js
@@ -69,4 +69,18 @@ app.delete('/api/posts/:id', (req, res, next) => {
     });
 });
 
+app.put('/api/posts/:id', (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content 
+    });
+    Post.updateOne({ _id: req.params.id }, post).then(result => {
+        console.log(result)
+        res.status(200).json({
+            message: 'update successful!'
+        })
+    })
+})
+
 module.exports = app;
