6aac842c0d039e14472e8799300f04208f90b75a
travis.vela <tvela@appddictionstudio.com>
2020-04-13 10:11:08 -0500

    add login method and send jwt to angular

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user')
 
@@ -23,10 +24,45 @@ router.post('/signup', (req, res, next) => {
         .catch(err => {
             res.status(500).json({
                 error: err
+            });
+        });
+});
+
+router.post('/login', (req, res, next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            } 
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message: 'Auth failed',
+                    error: err
+                })
+            }
+            const token = jwt.sign(
+                { email: fetchedUser.email, userId: fetchedUser._id },
+                'this_is_my_scecret',
+                { expiresIn: '1h' }
+            );
+            res.status(200).json({
+                token: token
             })
         })
-
-})
+        .catch(err => {
+            return res.status(401).json({
+                message: 'Auth failed',
+                error: err
+            })
+        })
+});
 
 
 module.exports = router;
