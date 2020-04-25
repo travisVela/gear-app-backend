4df7a7128435debb971841b0590cbfc72053a972
travis.vela <tvela@appddictionstudio.com>
2020-04-24 20:51:34 -0500

    update post and user routes

--- routes/posts.js
+++ routes/posts.js
@@ -100,7 +100,8 @@ router.put('/:id', checkAuth, (req, res, next) => {
         _id: req.body.id,
         title: req.body.title,
         content: req.body.content ,
-        imagePath: imagePath
+        imagePath: imagePath,
+        creator: req.userData.userId
     });
     Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
         console.log(result);
