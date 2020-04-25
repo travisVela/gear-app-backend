08da23ff6324357ef85437404db31d13fa875155
travis.vela <tvela@appddictionstudio.com>
2020-04-24 20:12:51 -0500

    add creator to the post model

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
     try {
         const token =  req.headers.authorization.split(' ')[1];
         console.log(req.headers.authorization);
-        jwt.verify(token, 'this_is_my_secret');
+        const decodedToken = jwt.verify(token, 'this_is_my_secret');
+        req.userData = { email: decodedToken, userId: decodedToken.userId };
         next();
     } catch (err) {
         res.status(401).json({
--- routes/posts.js
+++ routes/posts.js
@@ -33,7 +33,8 @@ router.post('', checkAuth, multer({storage: storage}).single('image'), (req, res
     const post = new Post({
         title: req.body.title,
         content: req.body.content,
-        imagePath: url + '/images/' + req.file.filename
+        imagePath: url + '/images/' + req.file.filename,
+        creator: req.userData.userId
     });
     console.log('🤖 post', post);
     post.save().then(result => {
@@ -75,11 +76,17 @@ router.get('/:id', (req, res, next) => {
 
 router.delete('/:id', checkAuth, (req, res, next) => {
 
-    Post.deleteOne({ _id: req.params.id }).then(result => {
+    Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
         console.log(result);
-        res.status(200).json({
-            message: 'Post deleted'
-        });
+        if (result.n > 0) {
+            res.status(200).json({
+                message: 'Delete successful'
+            });
+        } else {
+            res.status(401).json({
+                message: 'Not authorized'
+            })
+        }
     });
 });
 
@@ -95,10 +102,17 @@ router.put('/:id', checkAuth, (req, res, next) => {
         content: req.body.content ,
         imagePath: imagePath
     });
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
-        res.status(200).json({
-            message: 'update successful!'
-        });
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+        console.log(result);
+        if (result.nModified > 0) {
+            res.status(200).json({
+                message: 'update successful!'
+            });
+        } else {
+            res.status(401).json({
+                message: 'Not authorized'
+            })
+        }
     });
 });
 
